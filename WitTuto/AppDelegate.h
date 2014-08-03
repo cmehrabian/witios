7b3651bb70f4e56ad202e00ae950d82ce134b8b2
cmehrabian <Cameron@Camerons-MacBook-Pro.local>
2014-08-02 22:55:12 -0700

    Initial Commit

---
+++ WitTuto/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  WitTuto
+//
+//  Created by Cameron Mehrabian on 8/2/14.
+//  Copyright (c) 2014 ** *** **. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ WitTuto/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  WitTuto
+//
+//  Created by Cameron Mehrabian on 8/2/14.
+//  Copyright (c) 2014 ** *** **. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
